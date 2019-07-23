4082d07ca090d0f9ddbeba8312d4caccfc7f2bb6
Dane Cross <dncross@ucsc.edu>
2019-07-23 19:40:05 +0200

    initialize run build and source should work now

--- initialize_run_build.sh
+++ initialize_run_build.sh
@@ -1,23 +1,38 @@
 
+echo "\n\nPulling from SummerProject\n\n"
+cd ~/SummerProject
+git pull
+
 #remake Tools and Reconstruction and Analysis
 cd na62fw/
+echo "\n\nPulling from na62fw\n\n"
+git pull
+
 source NA62Reconstruction/scripts/env.sh
 cd NA62Tools
 make clean
 make 
-source scripts/env.sh
+cd ../NA62MC
+make clean
+make
+cd ../NA62Reconstruction
 make clean
 make
 
-~/SummerProject/na62fw/NA62FWBuildRunTree.py -v rcurrent -w ~/Analysis
-cd ~/Analysis
-source envAnalysis.sh
-NA62AnalysisBuilder.py prepare MyAnalysis
-cd MyAnalysis
+#~/SummerProject/na62fw/NA62FWBuildRunTree.py -v rcurrent -w ~/Analysis
+#cd ~/Analysis
+#source envAnalysis.sh
+#NA62AnalysisBuilder.py prepare MyAnalysis
+#cd MyAnalysis
+#source scripts/env.s
+
+rm -r ~/Analysis
+cd ../NA62Analysis
 source scripts/env.sh
 
-#NA62AnalysisBuilder.py cleanall
-#NA62AnalysisBuilder.py build ~/Analysis/config --shared
+
+NA62AnalysisBuilder.py cleanall
+NA62AnalysisBuilder.py config --shared
 
 cd ../../SummerProject/
 
--- initialize_run_source.sh
+++ initialize_run_source.sh
@@ -10,16 +10,23 @@ cd ../NA62Reconstruction
 source scripts/env.sh
 cd ../NA62MC
 source scripts/env.sh
-
-#prepare the user directory
-~/SummerProject/na62fw/NA62FWBuildRunTree.py -v rcurrent -w ~/Analysis
-cd ~/Analysis
-source envAnalysis.sh 
-NA62AnalysisBuilder.py prepare MyAnalysis
-cd MyAnalysis
+cd ../NA62Analysis
 source scripts/env.sh
 
-cd ../../SummerProject/
+
+#prepare the user directory
+#~/SummerProject/na62fw/NA62Analysis/NA62AnalysisBuilder.py -v rcurrent -w ~/Analysis
+#cd ~/Analysis
+#source envAnalysis.sh 
+#NA62AnalysisBuilder.py prepare MyAnalysis
+#cd MyAnalysis
+#source scripts/env.sh
+
+
+#cd ~/Analysis
+#source scripts/env.sh
+
+cd ../SummerProject/
 
 
 
